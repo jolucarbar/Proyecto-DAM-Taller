1b00cd0f445a111df3195523a3426cf3efa3a0c1
jolucarbar <joseluis.cardenas.barroso@gmail.com>
2026-01-02 20:35:29 +0400

    Creación proyecto en Netbeans

---
+++ AppTaller/src/main/java/com/joseluis/apptaller/AppTaller.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.joseluis.apptaller;
+
+/**
+ *
+ * @author joseluis
+ */
+public class AppTaller {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
